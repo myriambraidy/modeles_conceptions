b4467544c663c304b7281c7f1a4eb93311074e1e
myriambraidy <myriam.braidy1@gmail.com>
2024-10-14 21:46:50 +0300

    Add files via upload

---
+++ StrategyPatternExample2.java
@@ -0,0 +1,19 @@
+public class StrategyPatternExample2 {
+ public static void main(String[] args) {
+ int[] numbers = {5, 1, 4, 2, 8};
+ // Create a sorter with the BubbleSort strategy
+ Sorter sorter = new Sorter(numbers);
+ SortingStrategy bubbleSort = new BubbleSort(sorter);
+ sorter.setStrategy(bubbleSort);
+ // Perform a sort using the BubbleSort strategy
+ sorter.performSort();
+ // Switch to the QuickSort strategy and perform another sort
+ SortingStrategy quickSort = new QuickSort(sorter);
+ sorter.setStrategy(quickSort);
+ sorter.performSort();
+
+ SortingStrategy insertion = new sortInsertion(sorter) ; 
+ sorter.setStrategy(insertion) ; 
+ sorter.performSort() ; 
+ }
+}
